796c6928d504b5390f7ad01569d1c15b30cd6dd6
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-08 15:17:40 -0500

    Add profile grid view; Add ProfileView to the main tab view

--- InstagramClone/InstagramClone/Core/Profile/ProfileView.swift
+++ InstagramClone/InstagramClone/Core/Profile/ProfileView.swift
@@ -8,56 +8,83 @@
 import SwiftUI
 
 struct ProfileView: View {
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
     var body: some View {
-        VStack {
-            // MARK: - Header
-            VStack(spacing: 10) {
-                // MARK: - Pic & Stats
-                HStack {
-                    Image("black-panther-1")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
+        NavigationStack {
+            ScrollView {
+                // MARK: - Header
+                VStack(spacing: 10) {
+                    // MARK: - Pic & Stats
+                    HStack {
+                        Image("black-panther-1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(statCount: 215, statTitle: "Posts")
+                            UserStatView(statCount: 987, statTitle: "Followers")
+                            UserStatView(statCount: 1645, statTitle: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
                     
-                    Spacer()
+                    // MARK: - Name & Bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Chadwick Bozeman")
+                            .fontWeight(.semibold)
+                        Text("Wakanda Forever")
+                    }
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                     
-                    HStack(spacing: 8) {
-                        UserStatView(statCount: 215, statTitle: "Posts")
-                        UserStatView(statCount: 987, statTitle: "Followers")
-                        UserStatView(statCount: 1645, statTitle: "Following")
+                    // MARK: - Action Button
+                    Button(action: {
+                        print("DEBUG: Tapped Edit Profile")
+                    }, label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundStyle(Color(.black))
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 1)
+                            }
+                    })
+                    
+                    Divider()
+                }
+                
+                // MARK: - Post Grid View
+                LazyVGrid(columns: gridItems, spacing: 2) {
+                    ForEach(0 ... 15, id: \.self) { _ in
+                        Image("black-panther-1")
+                            .resizable()
+                            .scaledToFill()
                     }
                 }
-                .padding(.horizontal)
-                
-                // MARK: - Name & Bio
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Chadwick Bozeman")
-                        .fontWeight(.semibold)
-                    Text("Wakanda Forever")
-                }
-                .font(.footnote)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                // MARK: - Action Button
-                Button(action: {
-                    print("DEBUG: Tapped Edit Profile")
-                }, label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundStyle(Color(.black))
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 6).stroke(Color.gray, lineWidth: 1)
-                        }
-                })
-                
-                Divider()
             }
-            
-            // MARK: - Post Grid View
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        print("DEBUG: Tapped Nav Bar Button")
+                    }, label: {
+                        Image(systemName: "line.3.horizontal")
+                            .tint(Color(.black))
+                    })
+                }
+            }
         }
     }
 }
--- InstagramClone/InstagramClone/Core/TabBar/MainTabView.swift
+++ InstagramClone/InstagramClone/Core/TabBar/MainTabView.swift
@@ -22,7 +22,7 @@ struct MainTabView: View {
             Text("Notifications")
                 .tabItem { Image(systemName: "heart") }
             
-            Text("Profile")
+            ProfileView()
                 .tabItem { Image(systemName: "person") }
         }
         .tint(.black)
