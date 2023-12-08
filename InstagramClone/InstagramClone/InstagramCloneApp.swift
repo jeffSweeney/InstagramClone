cda08393d665445e12b208f8e0d64e9aff2dc6cc
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-08 12:04:43 -0500

    Build app template

---
+++ InstagramClone/InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Jeffrey Sweeney on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
