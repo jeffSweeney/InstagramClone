558ea7c3817002a6f1c74423016ff6568a76aabe
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-08 15:45:47 -0500

    Build the FeedCell

---
+++ InstagramClone/InstagramClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,90 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Jeffrey Sweeney on 12/8/23.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack {
+            // MARK: - Profile Image & Username
+            HStack {
+                Image("batman-2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("Batman")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            
+            // MARK: - Post Image
+            Image("batman-2")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // MARK: - Action Buttons
+            HStack(spacing: 8) {
+                Button(action: { print("DEBUG: Like Post") }, label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                })
+                
+                Button(action: { print("DEBUG: Comment on Post") }, label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+                })
+                
+                Button(action: { print("DEBUG: Share Post") }, label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                })
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .tint(.black)
+            
+            // MARK: - Likes Label
+            Text("23 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 2)
+            
+            // MARK: - Caption Label
+            HStack {
+                Text("batman ").fontWeight(.semibold) + // The '+' treats this as one big text component for clean multi-line spacing
+                Text("Test caption for now")
+            }
+            .font(.footnote)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+            
+            // MARK: - Timestamp
+            Text("6h ago")
+                .font(.footnote)
+                .foregroundStyle(Color.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 2)
+        }
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ InstagramClone/InstagramClone/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  InstagramClone
+//
+//  Created by Jeffrey Sweeney on 12/8/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
