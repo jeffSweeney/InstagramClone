d8561a04381337823a2f1985dcee8e77c4079a6d
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-08 12:34:02 -0500

    Build base tab view

---
+++ InstagramClone/InstagramClone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabView.swift
+//  InstagramClone
+//
+//  Created by Jeffrey Sweeney on 12/8/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem { Image(systemName: "house") }
+            
+            Text("Search")
+                .tabItem { Image(systemName: "magnifyingglass") }
+            
+            Text("Upload Posts")
+                .tabItem { Image(systemName: "plus.square") }
+            
+            Text("Notifications")
+                .tabItem { Image(systemName: "heart") }
+            
+            Text("Profile")
+                .tabItem { Image(systemName: "person") }
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
