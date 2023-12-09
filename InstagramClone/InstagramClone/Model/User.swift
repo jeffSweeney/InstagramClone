0bda14c5b661b0eda9f9cf52b8ae1e989907868b
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-09 12:00:04 -0500

    Build a User model; Dynamically populate SearchView with mock User data

---
+++ InstagramClone/InstagramClone/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  InstagramClone
+//
+//  Created by Jeffrey Sweeney on 12/9/23.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    var username: String
+    var profileImageUrl: String?
+    var fullname: String?
+    var bio: String?
+    let email: String
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, username: "batman", profileImageUrl: "batman-2", fullname: "Bruce Wayne", bio: "Gotham's Dark Knight", email: "batman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "venom", profileImageUrl: "venom-2", fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "ironman", profileImageUrl: "iron-man-1", fullname: "Tony Stark", bio: "Playboy & Billionaire", email: "ironman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "blackpanther", profileImageUrl: "black-panther-2", fullname: "T'Challa", bio: "King of the Dead", email: "blackpanther@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "spiderman", profileImageUrl: "spiderman", fullname: "Peter Parker", bio: "Bray Bray's FAVE", email: "spiderman@gmail.com")
+    ]
+}
