e7ff3b4dc01b5541a6b7c6cee3fce6b1c98e6242
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-12-11 14:34:28 -0500

    Build an UploadPostView with photo picker support

---
+++ InstagramClone/InstagramClone/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,69 @@
+//
+//  UploadPostView.swift
+//  InstagramClone
+//
+//  Created by Jeffrey Sweeney on 12/11/23.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct UploadPostView: View {
+    @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            // MARK: - Action Toolbar
+            HStack {
+                Button(action: {
+                    caption = ""
+                    viewModel.cancelUpload()
+                    tabIndex = 0 // Return back to Home tab in our MainTabView
+                }, label: {
+                    Text("Cancel")
+                })
+                
+                Spacer()
+                
+                Text("New Post")
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                Button(action: {
+                    print("DEBUG: Upload Post tapped")
+                }, label: {
+                    Text("Upload")
+                        .fontWeight(.semibold)
+                })
+            }
+            .foregroundStyle(.black)
+            .padding(.horizontal)
+                
+            // MARK: - Post Image & Caption
+            HStack(spacing: 8) {
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                TextField("Enter your caption...", text: $caption, axis: .vertical)
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .onAppear { imagePickerPresented.toggle() }
+        .photosPicker(isPresented: $imagePickerPresented, selection: $viewModel.selectedImage)
+    }
+}
+
+#Preview {
+    UploadPostView(tabIndex: .constant(0))
+}
